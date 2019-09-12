828bc968775e527a0aa304da73d41fe091076fd1
ZGame27152 <52026450+ZGame27152@users.noreply.github.com>
2019-09-12 14:16:56 +0700

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,31 @@
+﻿{
+	"version": 1568272409,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/tree111-sheet0.png",
+		"images/tree111-sheet1.png",
+		"images/tree111-sheet2.png",
+		"images/ground-sheet0.png",
+		"images/tool0-sheet0.png",
+		"images/tool2-sheet0.png",
+		"images/tool3-sheet0.png",
+		"images/tool4-sheet0.png",
+		"images/frame-sheet0.png",
+		"images/tool5-sheet0.png",
+		"images/tree2-sheet0.png",
+		"images/bg2-sheet0.png",
+		"images/tool1-sheet0.png",
+		"images/exit-sheet0.png",
+		"images/seed-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
